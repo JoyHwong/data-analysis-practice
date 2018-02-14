bea1efa375e8537d4db265bc3e56254e4350abc1
hdvsyu <hdvsyu@foxmail.com>
2018-02-14 16:27:10 +0800

    feat: draw windows and not windows figure

--- ch02/ch02.py
+++
@@ -1,9 +0,0 @@
-import json
-
-filename = "usagov_bitly_data2013-05-17-1368832207.txt"
-
-records = [json.loads(line) for line in open(filename, encoding='utf-8')]
-
-time_zones = [rec['tz'] for rec in records if 'tz' in rec]
-
-print(time_zones[:10])
